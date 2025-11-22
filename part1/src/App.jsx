de3ede584953d19828ea3ed8251e628f8a786cb8
Arufiand <alfiandannyarmanta@gmail.com>
2025-11-22 20:30:33 +0700

    feat: refactor App component to implement reset functionality

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -29,35 +29,46 @@ const History = (props) => {
 }
 
 const App = () => {
-    const [left, setLeft] = useState(0);
-    const [right, setRight] = useState(0);
-    const [allClicks, setAll] = useState([]);
-
-    const handleLeftClick = () => {
-        setAll(allClicks.concat('L'));
-        const updatedLeft = left + 1;
-        setLeft(updatedLeft);
-    }
-
-    const handleRightClick = () => {
-        setAll(allClicks.concat('R'));
-        const updatedRight = right + 1;
-        setRight(updatedRight);
-    }
+    const [value, setValue ] = useState(10);
 
     return (
-        <>
-            <div>
-                {left}
-                <Button onClick={handleLeftClick} text="left" />
-                <Button onClick={handleRightClick} text="right" />
-                {right}
-                <History allClicks={allClicks} />
-
-            </div>
-        </>
-    );
+        <div>
+            {value}
+            <Button onClick={() => setValue(1)}>Reset to zero</Button>
+        </div>
+    )
 }
+
+// const App = () => {
+//     const [left, setLeft] = useState(0);
+//     const [right, setRight] = useState(0);
+//     const [allClicks, setAll] = useState([]);
+//
+//     const handleLeftClick = () => {
+//         setAll(allClicks.concat('L'));
+//         const updatedLeft = left + 1;
+//         setLeft(updatedLeft);
+//     }
+//
+//     const handleRightClick = () => {
+//         setAll(allClicks.concat('R'));
+//         const updatedRight = right + 1;
+//         setRight(updatedRight);
+//     }
+//
+//     return (
+//         <>
+//             <div>
+//                 {left}
+//                 <Button onClick={handleLeftClick} text="left" />
+//                 <Button onClick={handleRightClick} text="right" />
+//                 {right}
+//                 <History allClicks={allClicks} />
+//
+//             </div>
+//         </>
+//     );
+// }
 
 
 export default App;
