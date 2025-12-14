9418fd0a285ceedf57f1b1d7fa16e9614750c992
Arufiand <alfiandannyarmanta@gmail.com>
2025-12-14 14:54:55 +0700

    feat: fetch persons
    Fetching persons through useEffect

--- part1/src/Exercise2.jsx
+++ part1/src/Exercise2.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import FilterComponent from '../components/filter.component.jsx'
 import PersonForm from '../components/personform.component.jsx'
 import Persons from '../components/persons.component.jsx'
+import axios from "axios";
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+
+    ]);
+
+    useEffect(() => {
+        async function fetchPersons () {
+            const personData = await axios.get('http://localhost:3001/persons');
+            setPersons(personData.data);
+        }
+        fetchPersons();
+    }, []);
+
     const [filter, setFilter] = useState('')
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -3,14 +3,9 @@ import App from './App2.jsx'
 import {StrictMode} from "react";
 import Exercise2 from "./Exercise2.jsx";
 
-import axios from 'axios'
-
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <StrictMode>
-    <App/>
-
-    <Exercise2 />
+        {/*<App/>*/}
+        <Exercise2 />
     </StrictMode>
 )
