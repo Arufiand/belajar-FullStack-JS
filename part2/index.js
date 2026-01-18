9e43ba48603cd726fe610b71a70c7b18c6f5d6ed
Arufiand <alfiandannyarmanta@gmail.com>
2026-01-18 11:51:34 +0700

    feat: add CRUD operations for phonebook API and enable HTTP header inspection in project config

--- part2/index.js
+++ part2/index.js
@@ -10,6 +10,29 @@ let notes = [
   { id: 3, title: "Third note", importance: true },
 ];
 
+let phonebook = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
@@ -59,6 +82,51 @@ app.post("/api/notes", (request, response) => {
   notes = notes.concat(note);
 
   response.status(201).json(note);
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(phonebook);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = phonebook.find((person) => person.id === id);
+  if (!person) {
+    return response.status(404).json({ error: "person not found" });
+  }
+  response.json(person);
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: "name and number required" });
+  }
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+  phonebook = phonebook.concat(person);
+  response.status(201).json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  phonebook = phonebook.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+app.put("/api/persons/:id", (request, response) => {
+  const body = request.body;
+  const id = Number(request.params.id);
+  const person = phonebook.find((person) => person.id === id);
+  if (!person) {
+    return response.status(404).json({ error: "person not found" });
+  }
+  person.name = body.name || person.name;
+  person.number = body.number || person.number;
+  response.json(person);
 });
 
 const PORT = 3001;
