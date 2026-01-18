2d1841c5fd1570b26a25743fb46623d56e6d46ba
Arufiand <alfiandannyarmanta@gmail.com>
2026-01-18 10:40:57 +0700

    feat: replace HTTP server with Express, add routes for root and notes API

--- part2/index.js
+++ part2/index.js
@@ -1,4 +1,5 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
 let notes = [
   { id: 1, title: "First note", importance: true },
@@ -6,9 +7,12 @@ let notes = [
   { id: 3, title: "Third note", importance: true },
 ];
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end(JSON.stringify(notes, null, 2));
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
 });
 
 const PORT = 3001;
