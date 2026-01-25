18325ed1db56f5a2d2b589bc8c57629e9fc4f241
Arufiand <alfiandannyarmanta@gmail.com>
2026-01-25 16:35:19 +0700

    feat: fix typo in `key` prop and add initial MongoDB integration with Mongoose

--- part1/components/notes.jsx
+++ part1/components/notes.jsx
@@ -1,7 +1,7 @@
 const NotesComponent = ({ note, toggleImportance }) => {
   const label = note.important ? "make not important" : "make important";
   return (
-    <li className="note" key={node.id}>
+    <li className="note" key={note.id}>
       {note.title}
       <button onClick={toggleImportance}>{label}</button>
     </li>
