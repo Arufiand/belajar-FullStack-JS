3abf33ad7f6653891325809aefce5fa5af9b5a85
Arufiand <alfiandannyarmanta@gmail.com>
2026-02-18 05:37:50 +0700

    feat: add JSHint and Prettier configuration files for improved code quality

--- part2/index.js
+++ part2/index.js
@@ -1,31 +1,28 @@
-'use strict'
-const express = require('express')
-const {
-  RequestLogger,
-  unknownEndpoint,
-  errorHandler,
-} = require('./request_logger')
-const app = express()
-const cors = require('cors')
-const notesRouter = require('./routes/note_router')
-const phonebookRouter = require('./routes/phonebook_router')
-const phonebookInfoRouter = require('./routes/phonebook_info_router')
+'use strict';
+const express = require('express');
+const { RequestLogger, unknownEndpoint } = require('./utils/request_logger');
 
-app.use(cors())
-app.use(express.json())
-app.use(RequestLogger)
-app.use('/api/notes', notesRouter)
-app.use('/api/persons', phonebookRouter)
-app.use('/api/info', phonebookInfoRouter)
+const app = express();
+const cors = require('cors');
+const notesRouter = require('./routes/note_router');
+const phonebookRouter = require('./routes/phonebook_router');
+const phonebookInfoRouter = require('./routes/phonebook_info_router');
 
-app.use(unknownEndpoint)
-app.use(errorHandler)
+app.use(cors());
+app.use(express.json());
+app.use(RequestLogger);
+app.use('/api/notes', notesRouter);
+app.use('/api/persons', phonebookRouter);
+app.use('/api/info', phonebookInfoRouter);
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+  response.send('<h1>Hello World!</h1>');
+});
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ part2/utils/request_logger.js
@@ -0,0 +1,24 @@
+const RequestLogger = (req, res, next) => {
+  const start = process.hrtime();
+
+  res.on('finish', () => {
+    const diff = process.hrtime(start);
+    const ms = diff[0] * 1e3 + diff[1] / 1e6; // milliseconds with sub-ms precision
+    console.log('Method', req.method);
+    console.log('Path', req.path);
+    console.log('Body', req.body);
+    console.log('Url', req.url);
+    console.log('Status code', res.statusCode);
+    console.log('Response Time', ms.toFixed(3) + ' ms');
+
+    const hdr = res.getHeader && res.getHeader('X-Response-Time');
+    if (hdr) {
+      console.log('X-Response-Time header:', hdr);
+    }
+    console.log('---');
+  });
+
+  next();
+};
+
+module.exports = { RequestLogger };
