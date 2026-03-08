b7f6dfc58e7ffa974115730a67e80c4699ac0b6b
Arufiand <alfiandannyarmanta@gmail.com>
2026-03-08 18:09:56 +0700

    feat: enhance logger to suppress output in test environment and add tests for note API

--- backend/tests/note_api.test.js
+++ backend/tests/note_api.test.js
@@ -1,4 +1,25 @@
-const { test, after } = require('node:test');
+const { test, after, describe } = require('node:test');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const assert = require('assert');
+
+const api = supertest(app);
+describe('note_api_test', () => {
+  test('notes are returned as json', async () => {
+    await api
+      .get('/api/notes')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
+
+  test('specific note is within the returned notes', async () => {
+    const respose = await api.get('/api/notes/');
+    const content = respose.body.map(note => note.content);
+    assert.strictEqual(content.includes('HTML is easy'), true);
+  });
+});
+
+after(async () => {
+  await mongoose.connection.close();
+});
--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -1,4 +1,10 @@
-const info = (...params) => console.info(...params);
-const error = (...params) => console.error(...params);
-const warn = (...params) => console.warn(...params);
+const info = (...params) => {
+  if (process.env.NODE_ENV !== 'test') console.info(...params);
+};
+const error = (...params) => {
+  if (process.env.NODE_ENV !== 'test') console.error(...params);
+};
+const warn = (...params) => {
+  if (process.env.NODE_ENV !== 'test') console.warn(...params);
+};
 module.exports = { info, error, warn };
