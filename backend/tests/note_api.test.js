47ffa270b2f0480768009c8837236f7183084a28
Arufiand <alfiandannyarmanta@gmail.com>
2026-03-08 23:42:35 +0700

    test: enhance note API tests with setup and additional cases

--- backend/tests/note_api.test.js
+++ backend/tests/note_api.test.js
@@ -1,22 +1,57 @@
-const { test, after, describe } = require('node:test');
+const { test, before, after, describe, beforeEach } = require('node:test');
 const mongoose = require('mongoose');
+require('dotenv').config();
+process.env.NODE_ENV = 'test';
 const supertest = require('supertest');
 const app = require('../app');
 const assert = require('assert');
+const Note = require('../models/notes');
+const { connectIfNeeded } = require('../utils/config');
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true
+  }
+];
 
 const api = supertest(app);
+
+before(async () => {
+  await connectIfNeeded(); // selects test DB when NODE_ENV === 'test'
+});
+beforeEach(async () => {
+  await Note.deleteMany({});
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+  noteObject = new Note(initialNotes[1]);
+  await noteObject.save();
+});
+
+/** @type {import('superagent').Response} */
 describe('note_api_test', () => {
-  test('notes are returned as json', async () => {
+  test.only('notes are returned as json', async () => {
     await api
       .get('/api/notes')
       .expect(200)
       .expect('Content-Type', /application\/json/);
   });
 
-  test('specific note is within the returned notes', async () => {
-    const respose = await api.get('/api/notes/');
-    const content = respose.body.map(note => note.content);
+  test.only('specific note is within the returned notes', async () => {
+    /** @type {import('superagent').Response} */
+    const response = await api.get('/api/notes/');
+    const content = response.body.map(note => note.content);
     assert.strictEqual(content.includes('HTML is easy'), true);
+  });
+
+  test.only('all note is returned', async () => {
+    /** @type {import('superagent').Response} */
+    const response = await api.get('/api/notes/');
+    assert.strictEqual(response.body.length, initialNotes.length);
   });
 });
 
